48586975c8368160b2cede1278a44bedd1b2f0dd
Samuel Bonner <bonner.sam@gmail.com>
2021-08-04 14:31:18 -0500

    PUSH now works

--- src/server/db/tasks.ts
+++ src/server/db/tasks.ts
@@ -1,55 +1,80 @@
 import { Query } from "./index";
+import "../routes/tasks";
 
 // This defines a function ("all") which will return all tasks using the Query function defined in index.ts
-// , users.name
 const allTasks = async () =>
     await Query(`
-    select tasks.id, tasks.title, tasks.details, tasks.difficulty, tasks.priority, tasks.completed
-    from tasks
-    join users on tasks.userid = users.id
+    SELECT tasks.id, tasks.title, tasks.details, tasks.difficulty, tasks.priority, tasks.completed
+    FROM tasks
+    JOIN users ON tasks.userid = users.id
 `);
 
 // This defines a function ("one") which will return one specific task using the Query function defined in index.ts
-// , 
+// ,
 const oneTask = async (id: string) =>
-    await Query(`
-    select tasks.id, tasks.title, tasks.details, tasks.difficulty, tasks.priority, tasks.completed
-    from tasks
-    join users on tasks.userid = users.id
-    where tasks.id = ?;
-`, [id]);
-
+    await Query(
+        `
+    SELECT tasks.id, tasks.title, tasks.details, tasks.difficulty, tasks.priority, tasks.completed
+    FROM tasks
+    JOIN users ON tasks.userid = users.id
+    WHERE tasks.id = ?;
+`,
+        [id]
+    );
 
 // Not sure if this is necessary, but this should return all tasks from ONE specific user (targeting userid)
 const allTasksFromUser = async (id: string) =>
-    await Query(`
-    select tasks.id, tasks.title, tasks.details, tasks.difficulty, tasks.priority, tasks.completed
-    from tasks
-    join users on tasks.userid = users.id
-    where tasks.userid = ?;
-`, [id]);
+    await Query(
+        `
+    SELECT tasks.id, tasks.title, tasks.details, tasks.difficulty, tasks.priority, tasks.completed
+    FROM tasks
+    JOIN users ON tasks.userid = users.id
+    WHERE tasks.userid = ?;
+`,
+        [id]
+    );
 
-const post = async (userid: string, name: string, details: string, difficulty: string, priority: string, completed: string) => Query(`
-    insert into tasks (userid, name, details, difficulty, priority, completed) values (?, ?, ?, ?, ?, ?)
-`, [userid, name, details, difficulty, priority, completed]);
+// insert into tasks (userid, title, details, difficulty, priority, completed) values (?, ?, ?, ?, ?, ?)
+// const insert = (taskDTO: {
+//     userid: string;
+//     title: string;
+//     details: string;
+//     difficulty: string;
+//     priority: string;
+//     completed: string;
+// }) => Query(`INSERT INTO tasks SET ?`, [taskDTO]);
 
-const put = async (id: string, newContent: string) => Query(`
+
+const insert = (userid: string, title: string, details: string, difficulty: string, priority: string, completed: string) => Query(`
+    INSERT INTO tasks (userid, title, details, difficulty, priority, completed) VALUES (?, ?, ?, ?, ?, ?)
+`, [userid, title, details, difficulty, priority, completed]);
+
+
+
+const put = async (id: string, newContent: string) =>
+    await Query(
+        `
     UPDATE tasks
     SET content = ?
     WHERE tasks.id = ?;
-`, [newContent, id]);
+`,
+        [newContent, id]
+    );
 
-const destroy = async (id: string) => Query(`
+const destroy = async (id: string) =>
+    await Query(
+        `
     DELETE FROM tasks WHERE tasks.id = ?;
-`, [id]);
-
+`,
+        [id]
+    );
 
 //These get exported, and then imported in index.ts as an object 'Tasks', and then exported again in index.ts
 export default {
     allTasks,
     oneTask,
     allTasksFromUser,
-    post,
+    insert,
     put,
-    destroy
+    destroy,
 };
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -7,7 +7,7 @@ router.get('/api/hello', (req, res, next) => {
     res.json('World');
 });
 
-// Node does not like the router.use below
+// Node does not like the router.use below -- Just kidding, works now
 router.use('/api/tasks', tasksRouter);
 
 export default router;
