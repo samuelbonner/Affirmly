c2dbbd99e64d1a34c9576ab0742122fd45825b22
Samuel Bonner <bonner.sam@gmail.com>
2021-08-08 13:23:28 -0500

    Added npm middleware 'morgan' for server request logging

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,15 @@
 import * as express from "express";
 import apiRouter from "./routes";
+import * as morgan from 'morgan';
 
 const app = express();
 
-app.use(express.json());
 
 app.use(express.static("public"));
 app.use(apiRouter);
+app.use(morgan('dev'));
+app.use(express.json());
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
