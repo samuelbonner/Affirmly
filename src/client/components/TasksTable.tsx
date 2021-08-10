cdf4d7a6bde741349ca1a51dcc532aee5325b78b
Samuel Bonner <bonner.sam@gmail.com>
2021-08-09 23:31:49 -0500

    EditTasks.tsx PUT and DELETE functionality works

--- src/client/components/TasksTable.tsx
+++ src/client/components/TasksTable.tsx
@@ -1,26 +1,22 @@
 import * as React from "react";
 import type { ITask } from "../../server/routes/tasks";
+import { Link } from "react-router-dom";
 
 const TasksTable: React.FC<TasksTableProps> = ({ task }) => {
     return (
-
-                <tbody>
-                    {/* Need to find a way to have a dynamically loaded className for difficulty / priority / completed
-                    Need to make this entire row clickable and call a TaskDetails component (need to write & link) using the task.id
-                    TaskDetails should show the full details of the task, not just the Title & Difficulty */}
-                    {/* Or should that be the EditTask component? */}
-                    {/* Code should be something like:
-                    <TaskDetails taskid={task.id} /> */}
-
-                    <tr className={`level-${task.difficulty}`}>
-                        <th scope="row">
-                            <input className="checkbox" type="checkbox" checked />
-                        </th>
-                        <td>{task.title}</td>
-                        {/* Need a way to dynamically check for task difficulty then return the appropriate trophy-in-progress for the below td */}
-                        <td>bronze</td>
-                    </tr>
-                </tbody>
+        <tbody>
+            <tr className={`level-${task.difficulty}`}>
+                <th scope="row">
+                    <input className="checkbox" type="checkbox" checked />
+                </th>
+                <td>{task.title}</td>
+                <td>
+                    <Link to={`/${task.id}/edittask`}>Edit</Link>
+                </td>
+                {/* Need a way to dynamically check for task difficulty then return the appropriate trophy-in-progress for the below td */}
+                <td>bronze</td>
+            </tr>
+        </tbody>
     );
 };
 
